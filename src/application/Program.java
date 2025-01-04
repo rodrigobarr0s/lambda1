f8295d592b07e3a2dd7309caebbc5ac1add8aed4
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-01-04 09:44:44 -0300

    Expressão lambda inline

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -18,12 +17,10 @@ public class Program {
 		list.add(new Product("Mouse", 50.0));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		
-		Double factor = 1.1;
-		
-		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
 
-		list.forEach(cons);
+		Double factor = 1.1;
+
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
 
 		list.forEach(System.out::println);
 	}
